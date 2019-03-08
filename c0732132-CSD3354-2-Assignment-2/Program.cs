bee3580d7c4d4d704cea9c2c6b3988503078d55d
visualstudio <visualstudio@122-PC26>
2019-03-08 18:19:45 -0500

    Lab-1

--- c0732132-CSD3354-2-Assignment-2/Program.cs
+++ c0732132-CSD3354-2-Assignment-2/Program.cs
@@ -12,11 +12,27 @@ namespace c0732132_CSD3354_2_Assignment_2
 //March 6, 2019
 
 {
-    class Program
 
+    class Program
     {
-        static void Main(string[] args)
+
+
+        public class DelegateExercises
         {
+            
+            public delegate void MyDelegate();
+            void Methodl()
+            {
+                
+                Console.WriteLine("Method1");
+                Console.ReadLine();
+            }
+
+            public void Method2()
+            {
+                MyDelegate myDelegate = new MyDelegate(Methodl);
+                myDelegate();
+            }
         }
     }
 }
