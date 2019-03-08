9c77366ab1863eab8bfd164aaad967138b61b44f
visualstudio <visualstudio@122-PC26>
2019-03-08 18:11:56 -0500

    team names

--- c0732132-CSD3354-2-Assignment-2/Program.cs
+++ c0732132-CSD3354-2-Assignment-2/Program.cs
@@ -5,6 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace c0732132_CSD3354_2_Assignment_2
+//Isha Garg c0732132
+//Dheeraj Jain c0723560
+//CSD3354 Section 3
+//Assignment2
+//March 6, 2019
+
 {
     class Program
 
