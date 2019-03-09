3a10db5c012019b144c97c9d52c80fad2d57329f
visualstudio <visualstudio@122-PC26>
2019-03-08 21:16:54 -0500

    lab 16

--- c0732132-CSD3354-2-Assignment-2/Program.cs
+++ c0732132-CSD3354-2-Assignment-2/Program.cs
@@ -17,44 +17,35 @@ namespace c0732132_CSD3354_2_Assignment_2
         public static void Main()
         {
             DelegateExercises delegateExercises = new DelegateExercises();
-            try
-            {
-                delegateExercises.Method3();
-                Console.ReadLine();
-            }
-            catch (System.Exception ex)
-            {
-                System.Console.WriteLine("Exception Occurred.");
-                Console.ReadLine();
-                delegateExercises.Method3();
-                Console.ReadLine();
-
-            }
+            delegateExercises.Method3();
+            Console.ReadLine();
         }
+
+
     }
+}
 
 
-    public delegate void MyDelegate(ref int intValue);
 
-    public class DelegateExercises
+public delegate void MyDelegate(ref int intValue);
+
+public class DelegateExercises
+{
+
+    void Method1(ref int intValue)
     {
 
-        void Method1(ref int intValue)
-        {
-            throw new System.Exception();
-            intValue = intValue + 5;
-            System.Console.WriteLine("Method1 " + intValue);
-        }
+        intValue = intValue + 5;
+        System.Console.WriteLine("Method1 " + intValue);
+    }
 
-        public void Method3()
-        {
-            MyDelegate myDelegate = new MyDelegate(Method1);
-
-            MyDelegate myDelegate1 = new MyDelegate(Method1);
-            MyDelegate myDelegate2 = myDelegate + myDelegate1;
-            int intParameter = 5;
-            myDelegate2(ref intParameter);
-        }
+    public void Method3()
+    {
+        MyDelegate myDelegate = new MyDelegate(Method1);
+        MyDelegate myDelegate1 = new MyDelegate(Method1);
+        MyDelegate myDelegate2 = myDelegate + myDelegate1;
+        int intParameter = 5;
+        myDelegate2(ref intParameter);
     }
 }
 
