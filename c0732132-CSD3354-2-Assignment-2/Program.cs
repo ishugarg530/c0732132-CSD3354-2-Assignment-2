3c33f2f370b064d99ce78003950f926152c20419
visualstudio <visualstudio@122-PC26>
2019-03-08 19:10:09 -0500

    Lab-04

--- c0732132-CSD3354-2-Assignment-2/Program.cs
+++ c0732132-CSD3354-2-Assignment-2/Program.cs
@@ -12,30 +12,43 @@ namespace c0732132_CSD3354_2_Assignment_2
 //March 6, 2019
 
 {
-        public class DelegateExercises
-        {
-            
-            public delegate void MyDelegate();
-            void Methodl(int i)
-            {
-                
-                Console.WriteLine("Method1");
-                Console.ReadLine();
-            }
+    public class DelegateExercises
 
-            public void Method2()
-            {
-                MyDelegate myDelegate = new MyDelegate(Methodl);
-                myDelegate();
-            }
+    {
+        public delegate int MyDelegate(int intValue);
+        public int Method1(int intMethod1)
+        {
+            return intMethod1 * 2;
         }
 
-    class Program
-    {
-        static void Main(string[] args)
+        public int Method2(int intMethod2)
         {
-            DelegateExercises delegateExercises = new DelegateExercises();
-            delegateExercises.Method2();
+            return intMethod2 * 10;
+        }
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            int result1 = myDelegate(10);
+            System.Console.WriteLine(result1);
+            myDelegate = new MyDelegate(Method2);
+            int result2 = myDelegate(10);
+            System.Console.WriteLine(result2);
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                DelegateExercises delegateExercises = new DelegateExercises();
+                delegateExercises.Method3();
+                Console.ReadLine();
+            }
         }
     }
 }
+
+
+
+
+
+        
